97182150a67ece547ca207fd12ebc25631f804a5
Vinicius Guedes <viniciusgued@gmail.com>
2019-07-26 12:59:02 -0300

    chore: get db host prefix through env vars

--- src/Main/Middlewares/DatabaseMiddleware.ts
+++ src/Main/Middlewares/DatabaseMiddleware.ts
@@ -3,10 +3,10 @@ import {Request, Response} from "express";
 
 export const getURLConnection = (): string => 
 {
-    let {DB_HOST, DB_NAME, DB_USER, DB_PASS, ENV} = process.env;
+    let {DB_HOST_PREFIX, DB_HOST, DB_NAME, DB_USER, DB_PASS, ENV} = process.env;
     if (ENV === "test") DB_NAME += "_test";
 
-    return `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+    return `${DB_HOST_PREFIX}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 };
 
 export const handle = (
